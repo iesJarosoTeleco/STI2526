2d6d3bec120e9bb5b4c3ac028288bb883f932d6e
iesJarosoTeleco <jdiajim841@g.educaand.es>
2025-10-01 12:19:53 +0200

    Add files via upload

---
+++ main/hello_world_main.c
@@ -0,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_timer.h"
+
+
+void app_main(void)
+{
+    gpio_config_t configLed1 = {};
+    
+    
+    
+    //gpio_config(const gpio_config_t *pGPIOConfig)
+}
